e9e3a399e09761d7253066c9fcf0edab9a5c09f5
root123 <651744031@qq.com>
2019-06-01 09:27:12 +0800

    -- 2019-06-01 拉取docker镜像url设置

--- linux/centos7/docker/03.执行程序shell脚本.sh
+++ linux/centos7/docker/03.执行程序shell脚本.sh
@@ -17,11 +17,3 @@
     ./hello.sh
 
 ----------------------------------------------------------------------
-# 新版的 Docker 使用 /etc/docker/daemon.json（Linux）
-# 或者 %programdata%\docker\config\daemon.json（Windows）来配置 Daemon。
-
-# 请在该配置文件中加入（没有该文件的话，请先建一个）：
-
-{
-  "registry-mirrors": ["https://docker.mirrors.ustc.edu.cn"]
-}
